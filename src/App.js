5c6b24fad6403608d9d297b4868b3899d991461b
pramod <pramod.rk@sonata-software.com>
2023-04-03 16:47:33 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect, useMemo, useCallback} from 'react';
+import { render } from 'react-dom';
+import { AgGridReact } from 'ag-grid-react'; // the AG Grid React Component
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS, always needed
+import 'ag-grid-community/styles/ag-theme-alpine.css'; // Optional theme CSS
+
+const App = () => {
+
+ const gridRef = useRef(); // Optional - for accessing Grid's API
+ const [rowData, setRowData] = useState(); // Set rowData to Array of Objects, one Object per Row
+
+ // Each Column Definition results in one Column.
+ const [columnDefs, setColumnDefs] = useState([
+   {field: 'userId', filter: true},
+   {field: 'Order ID', filter: true},
+   {field: 'price', cellRendererFramework: (param) => {
+                return (<select name="cars" id="cars">
+  <option value="volvo">Volvo</option>
+  <option value="saab">Saab</option>
+  <option value="mercedes">Mercedes</option>
+  <option value="audi">Audi</option>
+</select>)
+            }}
+ ]);
+
+ // DefaultColDef sets props common to all Columns
+ const defaultColDef = useMemo( ()=> ({
+     sortable: true
+   }));
+
+
+
+ // Example load data from server
+ useEffect(() => {
+   fetch('https://jsonplaceholder.typicode.com/todos/1')
+   .then(result => result.json())
+   .then(rowData => setRowData(rowData))
+ }, []);
+
+ 
+ return (
+   <div>
+
+
+     {/* On div wrapping Grid a) specify theme CSS Class Class and b) sets Grid size */}
+     <div className="ag-theme-alpine" style={{width: 500, height: 500}}>
+
+       <AgGridReact
+           ref={gridRef} // Ref for accessing Grid's API
+
+           rowData={rowData} // Row Data for Rows
+
+           columnDefs={columnDefs} // Column Defs for Columns
+           defaultColDef={defaultColDef} // Default Column Properties
+
+           animateRows={true} // Optional - set to 'true' to have rows animate when sorted
+           rowSelection='multiple' // Options - allows click selection of rows
+
+
+           />
+     </div>
+   </div>
+ );
+};
 
 export default App;
